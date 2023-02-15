0ac9108e0392e914d3d06488b7ce657dcfaa7b4e
Tyson <deppravacion@gmail.com>
2023-02-15 12:39:23 -0700

    node modules add

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
-import './components/radButtons/RadButtons'
-import RadioBase from './components/radioBase/RadioBase'
+import './components/welcome/Welcome'
+
+
 
 
 
@@ -10,36 +11,15 @@ class App extends React.Component {
     chooseLogIn: '',
   }
 
-  handleChange({ target: {name, value}} ) {
-    this.setState((prevState) => ({
-      ...prevState, 
-      [name]: value
-    }))
 
-  }
 
   render() {
-    const radioData = [
-      {
-        title: "Sign In", 
-        id: "signIn", 
-        type: "radio", 
-        name: "chooseLogIn", 
-        value: "signIn"
-      },
-      {
-        title: "Sign Up", 
-        id: "signUp", 
-        type: "radio", 
-        name: "chooseLogIn", 
-        value: "signUp"
-      }
-    ]
+
 
     return(
       <div>
-        <h1> Welcome to the store </h1>
-        <div>Sign In or continue as a Guest</div>
+        <Welcome />
+        
         
       </div>
     )
