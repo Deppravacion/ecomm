523b5886b59010d25f1fe8a84654a7c19ae31ae1
Tyson <deppravacion@gmail.com>
2023-03-11 13:16:22 -0700

    i miss OG

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,5 +1,8 @@
 import React from "react"
-import styles from './Cart.Module.css'
+import style from './Cart.module.css'
+import altbrick from '../../assets/altbrick.png'
+import build   from '../../assets/build.png'
+import { InputBase } from "../inputBase/InputBase"
 
 class Cart extends React.Component {
     state = {
@@ -7,19 +10,51 @@ class Cart extends React.Component {
     }
 
     render() {
+        const brickIcon = <div className={style.imgWrapper}><img src={altbrick} /></div>
+        const buildIcon = <div className={style.imgWrapper}><img src={build} /></div>
         return(
             <div className='container'>
             
-                <div className={styles.cartProgress}>//CART//shipping//payment//confirmation</div>
-                <div className={styles.mainCart}>
-                    <div className={styles.storeItems}>
-                        <div className={styles.item}>brick</div>
-                        <div className={styles.item}>mortar</div>
+                <div className={style.cartProgress}>//CART//shipping//payment//confirmation</div>
+                <h2>e.Market</h2>
+                <div className={style.mainCart}>
+
+                    <div className={style.storeItems}>
+                        <div className={style.item}>
+                            brick
+                            {brickIcon}
+                            <div className={style.qtyInput}>
+                                enter qty
+                            </div>
+                        </div>
+          
+                        <div className={style.item}>
+                            mortar
+                            {buildIcon}
+                            <div className={style.qtyInput}>
+                                <InputBase 
+                                    // id={}
+                                    onChange={this.localChange}
+                                    placeholder="qty"
+                                    name="mortar"
+                                />
+                                <div>$10 each / 80-lb bags</div>
+                                
+                            </div>
+                            <button className={style.addButton}>add</button>
+                        </div>
                     </div>
-                    <div className={styles.cartSummary}>
-                        ${this.state.totalPrice}
+
+                    <div className={style.cartSummary}>
+                        <h5>your cart totals</h5>
+                        <div className={style.total}>
+                        ${this.state.totalPrice}                            
+                        </div>
                     </div>
                 </div>
+                <div className={style.proceedButton}></div>
+
+                
         
             </div>
         )
